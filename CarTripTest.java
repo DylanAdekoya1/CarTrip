d8d5d06bf793a0e911b09e9a05a66b0b7de37f34
DylanAdekoya1 <59652161+DylanAdekoya1@users.noreply.github.com>
2020-02-03 08:30:45 -0500

    Create CarTripTest.java

---
+++ CarTripTest.java
@@ -0,0 +1,18 @@
+//Dylan Adekoya
+public class CarTripTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CarTrip c1= new CarTrip(12,14,18,5);
+		
+		System.out.println(c1);
+	}
+	
+	
+	
+
+}
